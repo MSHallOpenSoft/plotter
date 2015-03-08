cf80f4ccfb6dd2c890f4d589651294db45f68ee8
srinath <srinath9795@gmail.com>
2015-03-08 16:04:58 +0530

    String parsing done... now accepts exprs similar to xy=2 x^2-y^2=2 x(y-2)=2y latex code generation done... returns x,y values with the delta in x=0.02(default)

--- sympyParsing.py
+++ sympyParsing.py
@@ -3,39 +3,75 @@ import numpy
 x = symbols('x')
 y = symbols('y')
 z = symbols('z')
-# expr = x**3 + 4*x*y - z
 # print expr.subs([(x, 2),  (z, 0)])
+# Adaptive Mesh
+
+def symStr(str_expr):
+	indOfXor=str_expr.find('^')
+
+	# Changes x^2 to x**2
+	if indOfXor!=-1:
+		str_expr=str_expr.replace("^","**")
+
+	# Changes x=y to x-(y)... as sympy accepts only expressions which are equated to 0
+	indOfEq=str_expr.find('=')
+	indOfDEq=str_expr.find('==')
+	if indOfEq!=-1 and indOfEq!=indOfDEq:
+		splitExpr=str_expr.split('=')
+		if len(splitExpr)==2:
+			str_expr=splitExpr[0] + ' - ('+splitExpr[1]+')'
 
 
+	# Changes xy,yz,zx.. ,x(,..,x).. to x*y,...x*(,...)*x
+	strLis=list(str_expr)
+	i=1
+	while i<len(strLis):
+		if (strLis[i]=='x' or strLis[i]=='z' or strLis[i]=='y' or strLis[i].isdigit()) and (strLis[i-1]=='x' or strLis[i-1].isdigit() or  strLis[i-1]=='z' or strLis[i-1]=='y'):
+			strLis.insert(i,'*')
+		if (strLis[i]=='x' or strLis[i].isdigit() or strLis[i]=='z' or strLis[i]=='y') and (strLis[i-1]==')'):
+			strLis.insert(i,'*')
+		if (strLis[i]=='(') and (strLis[i-1]=='x' or strLis[i-1].isdigit() or strLis[i-1]=='z' or strLis[i-1]=='y'):
+			strLis.insert(i,'*')
+		i=i+1
+	return ''.join(strLis)
 
-print 'Enter the expression: ' ,
+def exprParseSolve(str_expr,x_start,x_end,x_step=0.02):
 
-str_expr = raw_input()
-indOfEq=str_expr.find('=')
-indOfDEq=str_expr.find('==')
-
-if indOfEq!=-1 and indOfEq!=indOfDEq:
-	splitExpr=str_expr.split('=')
-	if len(splitExpr)==2:
-		combinedStr=splitExpr[0] + ' - ('+splitExpr[1]+')'
-		expr=sympify(combinedStr)
-elif indOfEq==-1:
+	str_expr = symStr(str_expr)
 	expr = sympify(str_expr)	#Parses the string to sympy object
 
-
-
-print expr
-print 'Enter the values of x: ' ,
-
-num = map(int, raw_input().split())
-numArr=numpy.array(num)
-f = lambdify(x, expr, "numpy")
-findyRoot=f(numArr)
-if type(findyRoot) !='numpy.ndarray':
-	print solve(findyRoot,y)
-else:
+	# print 'Sympy expression is: ',expr
+	xArr=numpy.arange(x_start, x_end,x_step)
+	f = lambdify(x, expr, "numpy")
+	findyRoot=f(xArr)
+	# if type(findyRoot) !='numpy.ndarray':
+	# 	print solve(findyRoot,y)
+	# else:
+	# print findyRoot
+	yArr=[]
 	for exp in findyRoot:
-		print solve(exp,y)
-	
+		yArr.append( solve(exp,y) )
+	# print xArr
+	# print yArr
+
+	return xArr,yArr
+
+def exprLatex(str_expr):
+	str_expr = symStr(str_expr)
+	expr = sympify(str_expr)
+	return latex(expr)
+
+
+if __name__ == "__main__":
+	print 'Enter the expression: ' ,
+
+	str_expr = raw_input()
+
+	exprParseSolve(str_expr,1,4)
+
+
+
+
+
 # val=expr.subs(x, num[0])
 # print 'The value of expr at that num. is: ',val
